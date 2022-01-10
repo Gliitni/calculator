0e33d03e41d94ded68220af10dd70203e04ffbb9
Gliitni <linilorance298@gmail.com>
2022-01-10 12:43:48 +0700

    mm

---
+++ src/Mcal.js
@@ -0,0 +1,35 @@
+import React from'react';
+
+const Mcal =()=>{
+    return(
+        <div>
+            <div>
+                <input type="text" > </input>
+            </div>
+            <div>
+
+                <button>D</button>
+                <button>C</button>
+                <button>=</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>0</button>
+                <button>+</button>
+                <button>-</button>
+                <button>*</button>
+                <button>/</button>
+
+            </div>
+
+        </div>
+    )
+
+}
+export default Mcal;
